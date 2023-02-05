1f363f2cebf47118e2414f2500760f02fa22b6c4
sra <sara.habib48@gmail.com>
2023-02-05 18:10:37 +0200

    fixing type error

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,18 @@ import {PostCard , Categories , PostWidget} from '../components'
 import {getPosts} from '../services'
 
 
-export default function Home({ posts }) {
+export default function Home({ posts }: any) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
+          {posts.map((post : any, index: any) => (
             <PostCard key={index} post={post.node} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-            <PostWidget />
+            <PostWidget  />
             <Categories />
           </div>
         </div>
