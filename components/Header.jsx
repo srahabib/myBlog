4147cfca42b1e91cce0e0046db5a7de0c5bb4d5a
sra <sara.habib48@gmail.com>
2023-02-07 21:43:59 +0200

    adding a model as a footer

--- components/Header.jsx
+++ components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input type="search" style={{outline: "none"}} autocomplete="off" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-pink-500 focus:border-pink-500  " placeholder="Search Here..." required/>
+                <input type="search" style={{outline: "none"}} autoComplete="off" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-pink-500 focus:border-pink-500  " placeholder="Search Here..." required/>
                 <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-pink-400 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-4 py-2  ">Search</button>
             </div>
         </form>
@@ -55,7 +55,7 @@ const Header = () => {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input type="search" style={{outline: "none"}} autocomplete="off" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-pink-500 focus:border-pink-500 " placeholder="Search " required/>
+                <input type="search" style={{outline: "none"}} autoComplete="off" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-pink-500 focus:border-pink-500 " placeholder="Search " required/>
                 <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-pink-400 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
             </div>
         </form>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,45 @@ import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 import '../styles/global.scss'
 
+import * as THREE from 'three'
+import SceneInit from "../components/SceneInit"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Scene } from 'three'
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
+
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('./old_computers/scene.gltf', (gltf) => {
+
+      //gltf.scene.scale.set(0.1, 0.1, 0.1);
+      test.scene.add(gltf.scene);
+
+      // console.log(loadedModel);
+    });
+
+
+
+    // const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    // test.scene.add(boxMesh);
+  }, []);
+
   return (
     
     <Layout>
+      <canvas  id="myThreeJsCanvas">
+        
+      </canvas>
+      
       <Component {...pageProps} />
     </Layout>
     
